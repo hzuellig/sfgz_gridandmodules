04170672bfb00c83c342129a9f584de079d2706d
lasandola <74544081+lasandola@users.noreply.github.com>
2021-07-03 13:56:43 +0200

    Add files via upload

--- sario/js/main.js
+++ sario/js/main.js
@@ -1,14 +1,13 @@
-/* Open when someone clicks on the span element */
+/* Menu overlay */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav(y) {
     document.getElementById("myNav").style.width = "0%";
 }
 
-
+/* Audio */
 var audio = new Audio("Kunzite-Minerals-96.mp3");
 
 $('#play-pause-button').on("click",function(){
