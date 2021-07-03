8a793fed899892ffaa597deb30ca68a14cda3f60
HIMINN <73788974+HIMINN@users.noreply.github.com>
2021-07-03 15:00:11 +0200

    Add files via upload

---
+++ noah/process/tag6/javascript/js/play.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hzuellig on 02.07.21.
+ */
+
+function playAudio(){
+    function playAudio() {
+        let el = document.querySelector ('audio');
+        el.play();
+    }
+    
+}
