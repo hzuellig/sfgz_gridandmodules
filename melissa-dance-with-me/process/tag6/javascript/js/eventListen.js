921f18d52542fc1f465856f8ba1c0b0373ac7be8
MelissaThoma <73747780+MelissaThoma@users.noreply.github.com>
2021-07-07 18:18:16 +0200

    Delete melissa-dance-with-me directory

--- melissa-dance-with-me/process/tag6/javascript/js/eventListen.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    let articles = document.getElementsByTagName("article");
-    articles[0].setAttribute("class", "myclass");
-
-    let i;
-    for (i = 0; i < articles.length; i++) {
-        articles[i].setAttribute("class", "myclass");
-    }
-};
